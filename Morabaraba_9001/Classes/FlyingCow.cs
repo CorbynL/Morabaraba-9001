a14ecdbd0d5b22452384960291ed93fa8aae2388
Brian Welman <brianallisterwelman@gmail.com>
2018-04-12 16:28:51 +0200

    Add Flying cow class with construct and parameters

---
+++ Morabaraba_9001/Classes/FlyingCow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Morabaraba_9001.Classes
+{
+    class FlyingCow : Cow
+    {
+        public FlyingCow (int Position = -1, int PlayerID = -1) : base (Position, PlayerID)
+        {
+
+        }
+    }
+}
