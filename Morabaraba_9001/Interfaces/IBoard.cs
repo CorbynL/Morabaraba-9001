4769fb63610463c44035b4fe5f02cbc28d53faae
CorbynL <corbynlangley@gmail.com>
2018-04-12 15:41:25 +0200

    IBoard behavious

--- Morabaraba_9001/Interfaces/IBoard.cs
+++ Morabaraba_9001/Interfaces/IBoard.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Morabaraba_9001.Classes;
 
 namespace Morabaraba_9001
 {
     public interface IBoard
     {
+        IEnumerable<Cow> Cows { get; }
+
+        void Move(Cow cow, int Destination);
+
+        void KillCow(Cow cow);
+
+        void DrawBoard();
+
     }
 }
