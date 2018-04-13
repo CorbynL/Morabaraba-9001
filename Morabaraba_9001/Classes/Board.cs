1472ccfa9fe456644f425a94f4f6539c4f79d8d0
CorbynL <corbynlangley@gmail.com>
2018-04-13 17:15:37 +0200

    Implement DrawBoard

--- Morabaraba_9001/Classes/Board.cs
+++ Morabaraba_9001/Classes/Board.cs
@@ -1,32 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Morabaraba_9001.Classes
 {
     public class Board : IBoard
     {
-        public IEnumerable<Cow> Cows => throw new NotImplementedException();
+        public IEnumerable<Cow> Cows { get; private set; }
 
-        public Board() { }
+        private IEnumerable<string> BoardPopulation(IEnumerable<Cow> cows)
+        {
+            string[] a = cows.Select(x => (x.Symbol.ToString())).ToArray();
+            return a;
+        }
+
+        public Board()
+        {
+            initiliseCows();
+        }
+
+        private void initiliseCows()
+        {
+            Cows = new Cow[24];
+            for (int i = 0; i < 23; i++)
+            {
+                Cows = Cows.Select(x => new Cow(i, -1)).ToArray();
+            }
+        }
 
         public void DrawBoard()
         {
-            Console.WriteLine("\n\n\n\n\t     1     2     3     4     5     6     7\n\n"
-           + "\t A ({0}) ------------({1})-------------({2})    \n\n"
-           + "\t     |   \\             |        /       |    \n\n"
-           + "\t B   |   ({0})-------({1})-------({2})  |    \n\n"
-           + "\t     |     |  \\        |    /      |    |    \n\n"
-           + "\t C   |     |   ({0})-({1})-({2})   |    |    \n\n"
-           + "\t     |     |     |           |     |    |    \n\n"
-           + "\t D ({0})-({1})-({2})       ({3})-({4})-({5})\n\n"
-           + "\t     |     |     |           |     |    |    \n\n"
-           + "\t E   |     |   ({0})-({1})-({2})   |    |    \n\n"
-           + "\t     |     | /         |         \\ |    |    \n\n"
-           + "\t F   |   ({0})-------({1})-------({2})  |    \n\n"
-           + "\t     | /               |             \\  |    \n\n"
-           + "\t G ({0}) ------------({1})-------------({2})    ");
+            Console.WriteLine(String.Format(getBoardString(),(object[])BoardPopulation(Cows)));
             Console.ReadLine();
+        }
+
+        private string getBoardString()
+        {
+            StringBuilder builder = new StringBuilder("\n\n\n\n\t     1     2     3     4     5     6     7\n\n");
+            builder.Append("\t G   ({0}) -------------({1})---------------({2})\n\n");
+            builder.Append("\t     |   \\             |              /  |    \n\n");
+            builder.Append("\t B   |    ({3})---------({4})---------({5})    |\n\n");
+            builder.Append("\t     |     |  \\        |       /   |     |    \n\n");
+            builder.Append("\t C   |     |    ({6})---({7})---({8})    |     |    \n\n");
+            builder.Append("\t     |     |     |           |     |     |    \n\n");
+            builder.Append("\t D  ({9})---({10})---({11})         ({12})---({13})---({14})\n\n");
+            builder.Append("\t     |     |     |           |     |     |    \n\n");
+            builder.Append("\t E   |     |    ({15})---({16})---({17})    |     |    \n\n");
+            builder.Append("\t     |     | /         |         \\ |     |\n\n");
+            builder.Append("\t F   |    ({18})---------({19})---------({20})    |\n\n");
+            builder.Append("\t     | /               |              \\  |    \n\n");
+            builder.Append("\t G   ({21}) -------------({22})---------------({23})    ");
+            return builder.ToString();
         }
 
         public void KillCow(Cow cow)
