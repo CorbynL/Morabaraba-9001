2905874652c7681d682d10925831832f79fded8b
CorbynL <corbynlangley@gmail.com>
2018-04-13 17:10:37 +0200

    add Property to Cow: Symbol

--- Morabaraba_9001/Classes/Cow.cs
+++ Morabaraba_9001/Classes/Cow.cs
@@ -10,14 +10,33 @@ namespace Morabaraba_9001.Classes
 
         public int PlayerID { get; private set; }
 
+        public char Symbol { get; private set; }
+
         public Cow(int Position = -1, int PlayerID = -1)
         {
             this.Position = Position;
             this.PlayerID = PlayerID;
+            getSymbol();
+            
         }
         public void Move(int Destination)
         {
             Position = Destination;
+        }
+
+        private void getSymbol()
+        {
+            switch (PlayerID)
+            {
+                case 0: Symbol = 'R';
+                    break;
+                case 1: Symbol = 'B';
+                    break;
+                case -1: Symbol = ' ';
+                    break;
+                default: throw new ArgumentException("Given ID is invalid");
+
+            }
         }
     }
 }
