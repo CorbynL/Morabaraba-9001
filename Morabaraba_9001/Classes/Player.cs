84a1ca7dfabcefc0ef0358bfaf190ae0fd7ea4c6
CorbynL <corbynlangley@gmail.com>
2018-04-19 10:56:02 +0200

    initial player implementation

---
+++ Morabaraba_9001/Classes/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Morabaraba_9001.Interfaces;
+
+namespace Morabaraba_9001.Classes
+{
+    public class Player : IPlayer
+    {
+        public Color Color { get; private set; }
+
+        public Player(Color color)
+        {
+            Color = color;
+        }
+
+        public bool Place(int position, IBoard board)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Morabaraba_9001/Interfaces/IPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Morabaraba_9001.Interfaces
+{
+    public enum Color { Red, Blue };
+
+    public interface IPlayer
+    {
+        Color Color { get; }
+        bool Place(int position, IBoard board);
+    }
+}
