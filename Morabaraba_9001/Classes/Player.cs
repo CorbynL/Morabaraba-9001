6694c2080291b59caccf901a04757a49ca7ffdd7
CorbynL <corbynlangley@gmail.com>
2018-04-20 19:26:03 +0200

    add CowBox member and Place method to Player

--- Morabaraba_9001/Classes/Player.cs
+++ Morabaraba_9001/Classes/Player.cs
@@ -8,15 +8,18 @@ namespace Morabaraba_9001.Classes
     public class Player : IPlayer
     {
         public Color Color { get; private set; }
+        private ICowBox Box;
 
-        public Player(Color color)
+        public Player(Color color, ICowBox box)
         {
             Color = color;
+            Box = box;
         }
 
         public void Place(int position, IBoard board)
         {
-            throw new NotImplementedException();
+            ICow cow = Box.TakeCow(Color);
+            board.Place(cow, position);
         }
 
         public void Kill(int position, IBoard board)
