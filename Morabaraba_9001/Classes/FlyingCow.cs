c8f6c0b2d12b4e67d3eea24e7b2735b3c1ede689
CorbynL <corbynlangley@gmail.com>
2018-04-20 17:49:44 +0200

    use Color instead of int iD

--- Morabaraba_9001/Classes/FlyingCow.cs
+++ Morabaraba_9001/Classes/FlyingCow.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Morabaraba_9001.Interfaces;
 
 namespace Morabaraba_9001.Classes
 {
     class FlyingCow : Cow
     {
-        public FlyingCow (int Position = -1, int PlayerID = -1) : base (Position, PlayerID)
+        public FlyingCow (int Position = -1, Color Color = Color.Black) : base (Position, Color)
         {
 
         }
