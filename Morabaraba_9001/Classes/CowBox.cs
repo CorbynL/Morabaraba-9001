33045eb8cf0ffa6e1adc45ba2e5be32a067cf483
CorbynL <corbynlangley@gmail.com>
2018-04-20 19:26:46 +0200

    implement CowBox interface methods

--- Morabaraba_9001/Classes/CowBox.cs
+++ Morabaraba_9001/Classes/CowBox.cs
@@ -1,17 +1,36 @@
 ﻿using Morabaraba_9001.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Morabaraba_9001.Classes
 {
     public class CowBox : ICowBox
     {
+        private ICow[] Cows;
+
+        public CowBox()
+        {
+            Cows = new ICow[24];
+
+            for (int i = 0; i < 24; i++)
+            {
+                if (i < 12) { Cows[i] = new Cow(-1, Color.Red); }
+                else { Cows[i] = new Cow(-1, Color.Blue); }
+            }
+        }
+
         public int RemainingCows(Color c)
         {
-            throw new System.NotImplementedException();
+            return Cows.Select(x => x.Color == c).ToArray().Length;
         }
 
         public ICow TakeCow(Color c)
         {
-            throw new System.NotImplementedException();
+            ICow cow = Cows.First(x => x.Color == c);
+            List<ICow> list =  Cows.ToList();
+            list.Remove(cow);
+            Cows = list.ToArray();
+            return cow;
         }
     }
 }
