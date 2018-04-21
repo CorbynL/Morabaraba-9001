66b76c6ea6e0882057ddbf9e6ff730e1d5e50fd4
CorbynL <corbynlangley@gmail.com>
2018-04-21 14:23:28 +0200

    add GameSession factory

---
+++ Morabaraba_9001/Factories/GameSessionFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Morabaraba_9001.Interfaces;
+using Morabaraba_9001.Classes;
+
+namespace Morabaraba_9001.Factories
+{
+    public static class GameSessionFactory
+    {
+        public static IGameSession CreateGameSession()
+        {
+            ICowBox box = new CowBox();
+            IBoard board = new Board();
+            IPlayer P1 = new Player(Color.Red, box);
+            IPlayer P2 = new Player(Color.Blue, box);
+            IReferee referee = new Referee(board, box);
+            IGameSession gameSession = new GameSession(board, P1, P2, box, referee);
+            return gameSession;
+        }
+    }
+}
